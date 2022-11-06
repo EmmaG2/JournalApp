d8db7c5c3298aa66999e1b255663ac0cd01c0907
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-11-05 22:16:42 -0600

    created: navbar component

---
+++ src/journal/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import { LoginOutlined, MenuOutlined } from "@mui/icons-material";
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material";
+
+export const Navbar = ({ drawerWidth = 240 }) => {
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { sm: `${drawerWidth}px` },
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          edge="start"
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuOutlined />
+        </IconButton>
+
+        <Grid
+          container
+          direction={"row"}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography
+            variant="h6"
+            component="h1"
+            noWrap
+            sx={{ fontSize: "1.5rem", display: "flex", alignItems: "center" }}
+          >
+            Journal App
+          </Typography>
+          <IconButton color="error">
+            <LoginOutlined />
+          </IconButton>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
