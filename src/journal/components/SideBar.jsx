cc72ee294cdfce2ca6ec2e39414c6a5fc3b89bff
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-11-05 22:17:05 -0600

    created: sidebar component

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,80 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
+export const SideBar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component={"nav"}
+      sx={{
+        width: { sm: drawerWidth },
+        flexShrink: { sm: 0 },
+      }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap componnt="h1">
+            Emma ❤
+          </Typography>
+        </Toolbar>
+
+        <Divider />
+
+        <List>
+          {[
+            "enero",
+            "febrero",
+            "marzo",
+            "abril",
+            "mayo",
+            "junio",
+            "julio",
+            "agosto",
+            "septiembre",
+            "octubre",
+            "noviembre",
+            "diciembre",
+          ]
+            .map(
+              (month) =>
+                month.charAt(0).toUpperCase() + month.slice(1).toLowerCase()
+            )
+            .map((month, i) => (
+              <ListItem key={i} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <TurnedInNot />
+                  </ListItemIcon>
+
+                  <Grid container>
+                    <ListItemText
+                      primary={month}
+                      secondary={"Lorem Ipsum Dolor Sit Asimet"}
+                    ></ListItemText>
+                  </Grid>
+                </ListItemButton>
+              </ListItem>
+            ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
